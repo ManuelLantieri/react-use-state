8729c827597bbd42d9b6e05a33bc8816636d515f
manuel lantieri <manuel.lantieri02@gmail.com>
2025-02-06 13:08:03 +0100

    accordion state

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,14 +1,34 @@
 import Btn from "./Btn";
+import { useState } from "react";
+
 export default function Accordion({ id, title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleWin = () => {
+    setIsOpen((currentValue) => !currentValue);
+  };
   return (
     <div className="acc-container">
-      <div className="acc-head">
-        <h2>{title}</h2>
-        <Btn type="primary">+</Btn>
-      </div>
-      <div className="acc-body">
-        <p>{description}</p>
-      </div>
+      {isOpen ? (
+        <div className="acc-head colorTitle">
+          <h2>{title}</h2>
+          <button className="btn btnPrimary" onClick={toggleWin}>
+            {isOpen ? "-" : "+"}
+          </button>
+        </div>
+      ) : (
+        <div className="acc-head">
+          {" "}
+          <h2>{title}</h2>
+          <button className="btn btnPrimary" onClick={toggleWin}>
+            {isOpen ? "-" : "+"}
+          </button>
+        </div>
+      )}
+      {isOpen && (
+        <div className="acc-body">
+          <p>{description}</p>
+        </div>
+      )}
     </div>
   );
 }
