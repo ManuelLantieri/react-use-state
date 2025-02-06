0bc22e86dec5075550443de63ae4bf161e2b6dbd
manuel lantieri <manuel.lantieri02@gmail.com>
2025-02-06 12:53:57 +0100

    main accordion list

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
 import Btn from "./Btn";
-export default function Accordion() {
+export default function Accordion({ id, title, description }) {
   return (
     <div className="acc-container">
       <div className="acc-head">
-        <h2>Title</h2>
+        <h2>{title}</h2>
         <Btn type="primary">+</Btn>
       </div>
       <div className="acc-body">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum odio
-          dolores iste ad volupta
-        </p>
+        <p>{description}</p>
       </div>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
 import Accordion from "./Accordion";
+import Languages from "../data/Languages";
 export default function Main() {
   return (
     <main>
       <section className="accordions">
-        <Accordion />
+        {Languages.map((language) => {
+          return (
+            <Accordion
+              key={language.id}
+              title={language.title}
+              description={language.description}
+            />
+          );
+        })}
       </section>
     </main>
   );
